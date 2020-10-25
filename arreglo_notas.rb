f4dba05a032c13b6ea8653b7658cbcbff9541ae5
alfredopl <aalfredoplz@gmail.com>
2020-10-25 10:31:56 -0300

    Completed

--- arreglo_notas.rb
+++ arreglo_notas.rb
@@ -1,1 +1,16 @@
 notas = [5, 7, 1, 3, 5, 8, 9, 'N.A', 'N.A', 3]
+def promedio(arr)
+    acum=0
+    pro=0
+    cant=arr.count
+    arr.each do |i|
+        if i=='N.A'
+            i = 2.0
+            acum+= i
+        else
+            acum+=i
+        end
+    end
+    pro= acum / cant
+end
+puts(promedio(notas))
